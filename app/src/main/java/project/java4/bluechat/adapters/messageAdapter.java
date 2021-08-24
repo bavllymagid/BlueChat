7f2b206aa2fae92f873bbb90afd344a6d9712c34
shehab <shehab@gmail.com>
2021-08-24 17:45:48 +0200

    text size preference working, added icons to preferences

--- app/src/main/java/project/java4/bluechat/adapters/messageAdapter.java
+++ app/src/main/java/project/java4/bluechat/adapters/messageAdapter.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
+import androidx.preference.PreferenceManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,7 +52,8 @@ public class messageAdapter extends BaseAdapter {
         MessageViewHolder holder = new MessageViewHolder();
         LayoutInflater messageInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
         Message message = messages.get(i);
-
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+//        float textSize = sharedPreferences.getFloat(context.getString(R.string.pref_font_size_key), 4);
 
         if (message.isMyMessage()) { // this message was sent by us so let's create a basic chat bubble on the right
             convertView = messageInflater.inflate(R.layout.mymessage, null);
@@ -58,6 +62,8 @@ public class messageAdapter extends BaseAdapter {
             holder.name = (TextView) convertView.findViewById(R.id.tv_name);
             holder.messageBody.setText(message.getContent());
             holder.name.setText("Me");
+            holder.name.setTextSize(getFontSizeFromPref(sharedPreferences));
+            holder.messageBody.setTextSize(getFontSizeFromPref(sharedPreferences));
         } else { // this message was sent by someone else so let's create an advanced chat bubble on the left
             convertView = messageInflater.inflate(R.layout.theirmessage, null);
             holder.name = (TextView) convertView.findViewById(R.id.tv_name);
@@ -65,6 +71,8 @@ public class messageAdapter extends BaseAdapter {
             convertView.setTag(holder);
             holder.name.setText(AppDatabase.getDatabase(context).conversationOperations().get(message.getConvId()).getName());
             holder.messageBody.setText(message.getContent());
+            holder.name.setTextSize(getFontSizeFromPref(sharedPreferences));
+            holder.messageBody.setTextSize(getFontSizeFromPref(sharedPreferences));
         }
 
         return convertView;
@@ -74,5 +82,19 @@ public class messageAdapter extends BaseAdapter {
 
         public TextView name;
         public TextView messageBody;
+    }
+
+        private float getFontSizeFromPref(SharedPreferences sharedPreferences){
+        String fontSize = sharedPreferences.getString(context.getString(R.string.pref_font_size_key), "medium");
+        switch (fontSize){
+            case "small":
+                return 9;
+            case "medium":
+                return 16;
+            case "large":
+                return 28;
+            default:
+                return 12;
+        }
     }
 }
